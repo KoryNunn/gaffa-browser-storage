f99ab8a85257e2e316806c615b09e4ca584d3cfb
mauricebutler <maurice.butler@gmail.com>
2014-03-31 17:27:32 +1000

    added remove method

--- browserStorage.js
+++ browserStorage.js
@@ -10,7 +10,12 @@ BrowserStorage.prototype.trigger = function(parent, scope, event){
     var action = this,
         data = action.source.value;
 
-    switch(action.method.value){
+    if(!action.method.value){
+        console.warn('You should set the value of method.');
+        return;
+    }
+
+    switch(action.method.value.toLowerCase()){
         case "get":
             data = window[action.storageType.value + 'Storage'].getItem(action.source.value);
             if(data === 'undefined'){
@@ -21,6 +26,10 @@ BrowserStorage.prototype.trigger = function(parent, scope, event){
 
         case "set":
             window[action.storageType.value + 'Storage'].setItem(action.target.value, JSON.stringify(data));
+            break;
+
+        case "remove":
+            window[action.storageType.value + 'Storage'].removeItem(action.target.value);
             break;
     }
 
